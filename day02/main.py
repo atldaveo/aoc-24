e0f486c81bf07eb2ce1d5bb73684a501f81b7895
atldaveo <atldaveo@gmail.com>
2024-12-09 14:09:04 -0500

    Revised code solves for part 2 while moving the initial check into a function

--- day02/main.py
+++ day02/main.py
@@ -1,43 +1,65 @@
-safe_ctr = 0
-unsafe_ctr = 0
+safe_ctr = 0                        # Number of safe reports
+unsafe_ctr = 0                      # Number of unsafe reports
 
-# Open file and split by line
-# Each line is a 'report'
-with open("input.txt", "r") as file:
+# Is the report safe?
+def analyze_report(level):
+    report_status = 0               # 1 is safe, 0 is unsafe
+    is_increasing = True       
+    is_decreasing = True
+
+# Test if the report is increasing properly
+    for i in range(len(level) - 1):  
+        if not (level[i] < level[i + 1] and 1 <= (level[i + 1] - level[i]) <= 3):   # Is the report strictly increasing by 3 or less? 
+            is_increasing = False
+            break
+
+# Test if the report is decreasing properly 
+    for i in range(len(level) - 1):  
+        if not (level[i] > level[i + 1] and 1 <= (level[i] - level[i + 1]) <= 3):   # Is the report strictly decreasing by 3 or less?
+            is_decreasing = False
+            break 
+
+# Is the report safe or not?          
+    if is_increasing or is_decreasing:
+        report_status = 1
+    else:
+        report_status = 0
+
+    return report_status  
+
+# Determine if a previously unsafe report is actually safe
+# Can the originally unsafe report be made safe with only 1 exception? 
+def problem_dampener(level):
+    for i in range(len(level)):
+        new_report = level[:i] + level[i+1:]        # Append new list with everything up to i and everything past i (exclusionary)
+        correction = analyze_report(new_report)     # Re-analyze the new report
+        if (correction == 1):                   
+            new_status = 1
+            break
+        else:
+            new_status = 0
+
+    return new_status
+
+with open("input.txt", "r") as file:                # Open 'input.txt' in read-mode
+
+ 
     report = [list(map(int, report.split())) for report in file if report.strip()]
-    # line.split() splits each line into a list of strings separated by white space
+    # report.split() splits each line into a list of strings separated by white space
     # map(int, ...) converts each string created by line.split into an int
     # list(map(...)) converts the map object into a list 
-    # line.strip() ensures that empty lines are skipped 
-    
-# Figure out how many reports are unsafe
-# Each report has a list of numbers called 'levels' (separated by spaces)
-# Read first 'report' into a list
-for level in report:
-    if len(level) > 1:
-        is_increasing = all(x < y for x, y in zip(level, level[1:]))
-        is_decreasing = all(x > y for x, y in zip(level,level[1:]))
-        safe_iteration = all(abs(x - y) <= 3 for x, y in zip(level, level[1:]))
-        # zip() creates pairs of adjacent numbers for comparison
-        # all() checks if all adjacent pairs satisfy the condition
-        
-# To be considered safe...
-# Levels must ALL be either increasing or decreasing
-# AND
-# Any two adjacent levels differ by at least one and at most three
-# If levels are all increasing or decreasing... 
-# AND if sequential elements are increasing / decreasing by between 1 and 3 (inclusive)
-# Report is SAFE
-# Add 1 to 'safe_ctr'
-# Move onto next report
-        if ((is_increasing and safe_iteration) or (is_decreasing and safe_iteration)):
+    # report.strip() ensures that empty lines are skipped 
+
+for level in report:                                # For each line in 'input.txt'
+    report_status = analyze_report(level)
+    if (report_status == 0):
+        damp_status = problem_dampener(level)       # Double check with the dampener
+        if (damp_status == 1):
             safe_ctr += 1
-# Else, the report is UNSAFE
         else:
             unsafe_ctr += 1
     else:
-        unsafe_ctr += 1
-
+        safe_ctr += 1
+    
 print(f"There are: {safe_ctr} safe reports")
 print(f"There are: {unsafe_ctr} unsafe reports")
-
